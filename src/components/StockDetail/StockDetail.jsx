7aa9731bae4d32fd291de1383104b82b42e158f5
Wynn <wynn.blevins@apivista.com>
2020-05-11 16:28:44 -0400

    Stock advice no longer flips between buy and sell on initial page load

--- src/components/StockDetail/StockDetail.jsx
+++ src/components/StockDetail/StockDetail.jsx
@@ -10,24 +10,24 @@ export const StockDetail = (props) => {
       <table className="mui-table" data-test="detail-table">
         <tbody>
         <tr data-test="ticker-row">
-          <td>Symbol</td>
+          <td>Symbol:</td>
           <td>{props.stock.activeStock['1. symbol']}</td>
         </tr>
         <tr data-test="name-row">
-          <td>Name</td>
+          <td>Name:</td>
           <td>{props.stock.activeStock['2. name']}</td>
         </tr>
         <tr data-test="type-row">
-          <td>Type</td>
+          <td>Type:</td>
           <td>{props.stock.activeStock['3. type']}</td> 
         </tr>
         <tr data-test="region-row">
-          <td>Region</td>
+          <td>Region:</td>
           <td>{props.stock.activeStock['4. region']}</td> 
         </tr>
         <tr>
-          <td>Advice</td>
-          <td>{props.advice.prediction && props.advice.prediction.valueWillIncrease ? <p>Buy</p> : <p>Sell</p> }</td>
+          <td>Advice:</td>
+          <td>{props.advice.prediction ? (props.advice.prediction.valueWillIncrease ? <span>Buy</span> : <span>Buy</span>): <span>...loading</span> }</td> 
         </tr>
         </tbody>
       </table> 
